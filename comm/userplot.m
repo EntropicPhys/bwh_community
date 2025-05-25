36d6369368ef6c3b82c03517b42c936ce56eb776
Michel Ferré Díaz <75095570+EntropicPhys@users.noreply.github.com>
2025-05-25 14:59:01 +0300

    Delete comm directory

--- comm/userplot.m
+++
@@ -1,8 +0,0 @@
-function userplot(p,wnr) % userplot, called for p.plot.pstyle=-1; 
-% here just interface to uplot1,...,  depending on GLOBAL p2pglob.ps 
-global p2pglob; 
-switch p2pglob.ps 
-    case 1; uplot1(p,wnr); 
-    case 2; uplot2(p,wnr); 
-    case 3; uplot3(p,wnr);       
-end 
