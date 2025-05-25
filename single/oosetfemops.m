5f7b7d994276d4ebdcae8b59d62522f43bd3bc25
Michel Ferré Díaz <75095570+EntropicPhys@users.noreply.github.com>
2025-05-25 14:58:51 +0300

    Delete single directory

--- single/oosetfemops.m
+++
@@ -1,3 +0,0 @@
-function p=oosetfemops(p) % set FEM operators, homog. Neuman BC by default
-[K,M,~]=p.pdeo.fem.assema(p.pdeo.grid,1,1,1); % FEM matrices
-p.mat.K=K; p.mat.M=kron(diag([1,1,1]),M); % scalar Lapl., full M
